7a50cf2c2f7b2860a3e7e813711695c90773c145
Fredrick Vågen <57605516+FredVaagen@users.noreply.github.com>
2021-05-22 17:17:07 +0200

    Delete holidaze-project-exam-2-fredrick-vaagen-main directory

--- holidaze-project-exam-2-fredrick-vaagen-main/context/AppContext.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-//Appcontext to check if user is authenticated ->
-const AppContext = React.createContext({ isAuthenticated: false });
-export default AppContext;
--- holidaze-project-exam-2-fredrick-vaagen-main/next.config.js
+++
@@ -1,7 +0,0 @@
-
-module.exports = {
-    images: {
-      domains: ['holidaze-project-exam-2.s3.eu-north-1.amazonaws.com']
-    },
-  }
-
